e39d67165658b9047413b92ef77d254a1555edc3
lyhaithanh <88336836+lyhaithanh@users.noreply.github.com>
2023-07-11 23:04:58 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -116,7 +116,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=42000
+FIRST_PORT=40000
 LAST_PORT=42999
 
 gen_data >$WORKDIR/data.txt
