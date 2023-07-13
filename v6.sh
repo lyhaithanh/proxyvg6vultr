bd1c10e335b8a1fedcead55f7a334f02780c5a93
lyhaithanh <88336836+lyhaithanh@users.noreply.github.com>
2023-07-13 15:48:13 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -17,14 +17,12 @@ install_3proxy() {
     echo "installing 3proxy"
     mkdir -p /3proxy
     cd /3proxy
-    # URL="https://it4.vn/0.9.3.tar.gz"
     URL="https://raw.githubusercontent.com/hoan277/multi_proxy_ipv6/main/0.9.3.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.3
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    # wget https://it4.vn/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
     wget https://raw.githubusercontent.com/hoan277/multi_proxy_ipv6/main/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
     cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
     systemctl link /usr/lib/systemd/system/3proxy.service
